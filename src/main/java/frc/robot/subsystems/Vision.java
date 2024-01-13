0395922c2e295edc815eb538a05f323b654551c2
RealRedRobot <romrobot2000@gmail.com>
2024-01-13 10:43:07 -0600

    vision lib

--- src/main/java/frc/robot/subsystems/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -2,6 +2,6 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class Intake extends SubsystemBase{
+public class Vision extends SubsystemBase {
     
 }
