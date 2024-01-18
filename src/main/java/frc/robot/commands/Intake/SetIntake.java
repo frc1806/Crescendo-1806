e877d0002721195c59df99bb0f0e3b6cfef88431
Nathan Fletcher <nef111605@yahoo.com>
2024-01-17 18:46:31 -0600

    fixed intake command

--- src/main/java/frc/robot/commands/Intake/SetIntake.java
+++ src/main/java/frc/robot/commands/Intake/SetIntake.java
@@ -1,20 +1,24 @@
 package frc.robot.commands.Intake;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.DriverControls;
 import frc.robot.subsystems.Intake;
 
 public class SetIntake extends Command{
 
     private Intake mIntake;
+    private DriverControls mDriverControls;
     private double mSpeed;
 
-    public SetIntake(Intake intake, double speed){
+    public SetIntake(Intake intake, DriverControls driverControls, double speed){
         mIntake = intake;
+        mDriverControls = driverControls;
         mSpeed = speed;
     }
 
     @Override
     public void end(boolean interrupted) {
+        mIntake.setIntake(0);
     }
 
     @Override
@@ -28,7 +32,7 @@ public class SetIntake extends Command{
 
     @Override
     public boolean isFinished() {
-        return false;
+        return !mDriverControls.intake();
     }
 
 }
