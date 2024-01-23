aace301652ffb61d5d405e4c8c602ce225c1a372
RealRedRobot <romrobot2000@gmail.com>
2024-01-22 21:18:17 -0600

    pid tuned again

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -23,7 +23,7 @@ public class Constants {
     public static final double kMaxModuleSpeed = 4.0;
     public static final double kDriveBaseRadius = 6.8125;
     // Translation PID
-    public static final double kSwerveAutoPIDP = 3.0;
+    public static final double kSwerveAutoPIDP = 2.5;
     public static final double kSwerveAutoPIDI = 0.0;
     public static final double kSwerveAutoPIDD = 0.001;
 
