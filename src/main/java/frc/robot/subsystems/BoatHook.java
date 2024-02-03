37ea9aab0168954f822c2ff25fc30203944edabf
RealRedRobot <romrobot2000@gmail.com>
2024-02-03 14:52:19 -0600

    fixed angler

--- src/main/java/frc/robot/subsystems/BoatHook.java
+++ src/main/java/frc/robot/subsystems/BoatHook.java
@@ -1,12 +1,18 @@
 package frc.robot.subsystems;
 
+
+import com.revrobotics.CANSparkMax;
+
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.RobotMap;
 
 public class BoatHook extends SubsystemBase{
+    
+    private CANSparkMax mBoatHookMotor;
 
     public BoatHook(){
-        
+        mBoatHookMotor = new CANSparkMax(RobotMap.kBoatHookMotorPort, CANSparkMax.MotorType.kBrushless);
     }
 
     @Override
